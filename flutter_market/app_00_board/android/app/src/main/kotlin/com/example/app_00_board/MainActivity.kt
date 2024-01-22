4e022524b054d7617998f7581f6c68eb85ed1a2f
statusquo00 <jh.2144.9679@gmail.com>
2024-01-23 01:36:21 +0900

    개발환경세팅

---
+++ flutter_market/app_00_board/android/app/src/main/kotlin/com/example/app_00_board/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_00_board
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_market/app_00_board/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key:key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BoardPage(),
+    );
+  }
+}
+
+class BoardPage extends StatelessWidget {
+  const BoardPage({Key? key}): super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildProfileAppBar(),
+      
+    );
+  }
+}
+
+// 앱바생성 함수
+AppBar _buildProfileAppBar() {
+  return AppBar(
+    
+  );
+}
